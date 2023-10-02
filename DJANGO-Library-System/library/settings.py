f2a2d6e459a436d773c54d3f66c2772df5c089ed
rodriguezroberto <iamrobertorodriguez@proton.me>
2023-10-01 19:44:02 -0600

    fix

--- DJANGO-Library-System/library/settings.py
+++ DJANGO-Library-System/library/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-tm1(0_jqre1cv@v=b2yxzqz5z9+3p2lma9u-t0mbusxr%1_g*0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
